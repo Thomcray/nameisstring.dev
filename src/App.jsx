8fea8e9349a61d25ab3bd9ec0aab7d2ca997f238
Terna Nanev <ternathompson2@gmail.com>
2025-04-24 11:37:32 +0100

    lazy load app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
-import { lazy, Suspense } from "react";
 import Layout from "./Pages/Layout";
-import Loader from "./components/Loader";
 
 function App() {
-  const Home = lazy(() => import("./Pages/Layout"));
-  return (
-    <Suspense fallback={<Loader />}>
-      <Home />
-    </Suspense>
-  );
+  return <Layout />;
 }
 
 export default App;
--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,31 +1,34 @@
-import { Box } from "@chakra-ui/react";
-import Contact from "@/components/Contact";
-import Experience from "@/components/Experience";
+import { Box, Loader } from "@chakra-ui/react";
+import { lazy, Suspense } from "react";
 import Header from "@/components/Header/Header";
-import Projects from "@/components/Projects";
-import About from "@/components/About";
 
 function Layout() {
+  const About = lazy(() => import("@/components/About"));
+  const Projects = lazy(() => import("@/components/Projects"));
+  const Experience = lazy(() => import("@/components/Experience"));
+  const Contact = lazy(() => import("@/components/Contact"));
   return (
-    <Box
-      w={{
-        lg: "62em",
-        xl: "80em",
-        "2xl": "96em",
-      }}
-      h="dvh"
-      m="auto"
-      py={4}
-      spaceY={8}
-      display="flex"
-      flexDirection="column"
-    >
-      <Header />
-      <About />
-      <Projects />
-      <Experience />
-      <Contact />
-    </Box>
+    <Suspense fallback={<Loader />}>
+      <Box
+        w={{
+          lg: "62em",
+          xl: "80em",
+          "2xl": "96em",
+        }}
+        h="dvh"
+        m="auto"
+        py={4}
+        spaceY={8}
+        display="flex"
+        flexDirection="column"
+      >
+        <Header />
+        <About />
+        <Projects />
+        <Experience />
+        <Contact />
+      </Box>
+    </Suspense>
   );
 }
 
