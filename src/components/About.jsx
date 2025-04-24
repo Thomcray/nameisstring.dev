929ab8466d89b4ab30ed7eebbe9c2ca414b3a457
Terna Nanev <ternathompson2@gmail.com>
2025-04-24 23:06:55 +0100

    add spin border

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,12 @@
+import { keyframes } from "@emotion/react";
 import { Box, Image, Text } from "@chakra-ui/react";
 import myPicture from "@/assets/nameisstring.png";
 import Skills from "./Skills";
 
+const spin = keyframes`
+from {ntransform: rotate(0deg); }
+to { transform: rotate(360deg);b}
+`;
 function About() {
   return (
     <Box
@@ -27,23 +32,59 @@ function About() {
         spaceY={4}
       >
         <Box
-          w={{ base: "250px", md: "350px", xl: "350px" }}
-          h={{ base: "250px", md: "250px", xl: "350px" }}
+          position="relative"
+          w={{ base: "290px", md: "390px", xl: "390px" }}
+          h={{ base: "290px", md: "290px", xl: "390px" }}
           display="flex"
           justifyContent="center"
           alignItems="center"
-          borderRadius="full"
-          overflow="hidden"
-          border="md"
-          borderColor="gray.200"
         >
-          <Image
-            src={myPicture}
-            alt="nameisstring-image"
-            objectFit="cover"
-            boxSize="100%"
-            loading="lazy"
+          {/* Outer spinning ring */}
+          <Box
+            px={4}
+            py={4}
+            borderRadius="full"
+            borderWidth="4px"
+            borderStyle="solid"
+            borderColor="transparent"
+            borderRightColor="orange.500/40"
+            borderLeftColor="orange.500/40"
+            animation={`${spin} 5s linear infinite`}
+            _hover={{
+              transform: "scale(1.05)",
+              animationPlayState: "paused",
+            }}
+            display="inline-flex"
+            justifyContent="center"
+            alignItems="center"
+            position="absolute"
+            w="100%"
+            h="100%"
+            zIndex={0}
           />
+
+          {/* Static image container */}
+          <Box
+            w={{ base: "250px", md: "350px", xl: "350px" }}
+            h={{ base: "250px", md: "250px", xl: "350px" }}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            borderRadius="full"
+            overflow="hidden"
+            borderWidth="2px"
+            borderColor="gray.200"
+            bg="white"
+            zIndex={1}
+          >
+            <Image
+              src={myPicture}
+              alt="nameisstring-image"
+              objectFit="cover"
+              boxSize="100%"
+              loading="lazy"
+            />
+          </Box>
         </Box>
         <Box
           w={{ base: "full", md: "full", xl: "1/2" }}
